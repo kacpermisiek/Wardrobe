b40c71aafe5e8b3e23293ffc2e1a9f73184b02d8
kacpermisiek <315560@uwr.edu.pl>
2022-08-24 16:22:35 +0200

    Create RentEvent model

--- wardrobe/stuff/models.py
+++ wardrobe/stuff/models.py
@@ -9,19 +9,15 @@ BADGE_STATUSES = {'Dostępny': 'success',
                   'Niedostępny': 'dark'}
 
 
+def _get_statuses():
+    return tuple([(key, key) for key in BADGE_STATUSES.keys()])
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-
-
-def _get_badge_status(status):
-    return BADGE_STATUSES[status]
-
-
-def _get_statuses():
-    return tuple([(key, key) for key in BADGE_STATUSES.keys()])
 
 
 class Item(models.Model):
@@ -30,13 +26,19 @@ class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     status = models.CharField(max_length=20, choices=_get_statuses(), default='Dostępny')
-    badge_status = 'success'
 
     borrower = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     date_added = models.DateField(default=now)
-    date_reserved = models.DateField(null=True, blank=True)
-    date_borrowed = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.name} item from {self.category} category'
+
+
+class RentEvent(models.Model):
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    taken = models.BooleanField(default=False)
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- wardrobe/stuff/views.py
+++ wardrobe/stuff/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from stuff.models import Item
+from stuff.models import Item, BADGE_STATUSES
 
 
 def home(request):
+
+    def _get_badge_status(status):
+        return BADGE_STATUSES[status]
+
+    def _set_badge_statuses(items):
+        for item in items:
+            try:
+                item.badge_status = _get_badge_status(item.status)
+            except KeyError:
+                item.badge_status = 'dark'
+
+        return items
+
+    def _get_items():
+        items = Item.objects.all()
+
+        return _set_badge_statuses(items)
+
     context = {
-        'stuff': Item.objects.all(),
-        'title': 'Home page'
+        'stuff': _get_items(),
+        'title': 'strona główna'
     }
     return render(request, 'stuff/home.html', context)
 
