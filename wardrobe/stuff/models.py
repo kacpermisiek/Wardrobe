76eec829d43c78e920938d59c943cadc707a9945
kacpermisiek <315560@uwr.edu.pl>
2022-08-24 16:27:40 +0200

    add __str__ method to RentEvent class

--- wardrobe/stuff/models.py
+++ wardrobe/stuff/models.py
@@ -42,3 +42,7 @@ class RentEvent(models.Model):
 
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Rent event\t item:{self.item}\tuser: {self.user}\n' \
+               f'from {self.start_date} to {self.end_date}'
