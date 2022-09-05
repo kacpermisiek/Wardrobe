e8c035675626175dceeffe31a75586ce3377563c
kacpermisiek <315560@uwr.edu.pl>
2022-09-05 20:22:30 +0200

    Add registration form with validation and change language of website to polish

---
+++ wardrobe/users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def register(request):
+    def _request_method_is_post(method):
+        return method == 'POST'
+
+    def _proceed_redirection(form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        messages.success(request, f'Konto stworzone dla {username}')
+        return redirect('stuff-home')
+
+    if _request_method_is_post(request.method):
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            return _proceed_redirection(register_form)
+
+    else:
+        register_form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': register_form})
