7a832aa330c6844b43c0c3280999f26650ccb54c
kacpermisiek <315560@uwr.edu.pl>
2022-09-06 20:41:47 +0200

    Add cripsy forms style and add first name and last name to user registration

--- wardrobe/wardrobe/settings.py
+++ wardrobe/wardrobe/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'stuff.apps.StuffConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
