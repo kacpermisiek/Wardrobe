b8bd4910ebe5036434351ed088294dcf443fa92b
kacpermisiek <56176866+kacpermisiek@users.noreply.github.com>
2022-09-10 17:35:10 +0200

    Add UserUpdateForm and ProfileUpdateForm

--- wardrobe/users/models.py
+++ wardrobe/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,3 +10,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'Profile of {self.user.username}'
 
+    def save(self, *args, **kwargs):
+        super().save()
+
+        image = Image.open(self.image.path)
+
+        if image.height > 300 or image.width > 300:
+            output_size = (300, 300)
+            image.thumbnail(output_size)
+            image.save(self.image.path)
