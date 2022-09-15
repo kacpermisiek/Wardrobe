aef6659f481274bf0f52b4ceb81d95d877347df2
kacpermisiek <315560@uwr.edu.pl>
2022-09-15 22:28:24 +0200

    Done email password reset

--- wardrobe/wardrobe/urls.py
+++ wardrobe/wardrobe/urls.py
@@ -11,23 +11,26 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', authorization_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authorization_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/',
-         authorization_views.PasswordResetView.as_view(
-             template_name='users/password_reset.html'),
-         name='password_reset'
-         ),
-    path('password-reset/done/',
-         authorization_views.PasswordResetDoneView.as_view(
-             template_name='users/password_reset_done.html'),
-         name='password_reset_done'
-         ),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         authorization_views.PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'
-         ),
+
     path('profile/', user_views.profile, name='profile'),
 ]
+
+password_reset_urls = [
+    path('password-reset/',
+         authorization_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done',
+         authorization_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-complete',
+         authorization_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         authorization_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+]
+
+urlpatterns += password_reset_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
