83dea8125ea8dd9c79e922ab7e30291404f9385f
Kacper Misiek <315560@uwr.edu.pl>
2023-01-29 10:08:46 +0100

    Email hotfix

--- wardrobe/stuff/views.py
+++ wardrobe/stuff/views.py
@@ -27,7 +27,7 @@ from utils.forms_functions import request_method_is_post
 
 SUPER_USERS_EMAILS = ["315560@uwr.edu.pl"]
 if os.environ.get('RADWAS_EMAIL'):
-    SUPER_USERS_EMAILS += os.environ.get('RADWAS_EMAIL')
+    SUPER_USERS_EMAILS.append(os.environ.get('RADWAS_EMAIL'))
 
 
 def home(request):
