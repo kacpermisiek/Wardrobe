6c2a7793cfa71eb98d0a47353cef89563dc5e148
kacpermisiek <56176866+kacpermisiek@users.noreply.github.com>
2023-04-03 08:04:20 +0200

    Update settings.py

--- wardrobe/wardrobe/settings.py
+++ wardrobe/wardrobe/settings.py
@@ -137,7 +137,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'issp2021website@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'sgtdipkzcbqbyybn'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
